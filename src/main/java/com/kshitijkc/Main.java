b7dd54b7d57d4eb7ab79eafe600deb1333f5725f
Kshitij Kumar <kshitijkumarchoudhary@gmail.com>
2020-05-20 08:42:41 +0530

    making stage screen independent

--- src/main/java/com/kshitijkc/Main.java
+++ src/main/java/com/kshitijkc/Main.java
@@ -50,8 +50,8 @@ public class Main extends Application {
         stage.initOwner(primaryStage);
         stage.setX(primaryScreenBounds.getWidth() * (1.0 - (250.0 / 1366.0) - (20.0 / 1366.0))); // 1 - 0.18301610541 - 0.01464128843 && 0.01464128843 = 20 / 1366
         stage.setY((primaryScreenBounds.getHeight() * (1.0 - (425.0 / 768.0)))/2.0); // (1 - 0.55338541666) / 2 && set it as the middle of the screen
-//        stage.setWidth(primaryScreenBounds.getWidth() * (250.0 / 1366.0)); // 0.18301610541 = 250 / 1366
-//        stage.setHeight(primaryScreenBounds.getHeight() * (425.0 / 768.0)); // 0.55338541666 = 425 / 768
+        stage.setWidth(primaryScreenBounds.getWidth() * (250.0 / 1366.0)); // 0.18301610541 = 250 / 1366
+        stage.setHeight(primaryScreenBounds.getHeight() * (425.0 / 768.0)); // 0.55338541666 = 425 / 768
         stage.setResizable(false);
         stage.setAlwaysOnTop(true);
         stage.initStyle(StageStyle.TRANSPARENT);
